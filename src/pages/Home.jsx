1ba3397d691111c1f14a880159441c8d1440d842
mrzkhrmn <mrzkhrmn@hotmail..com>
2025-06-17 14:20:31 +0300

    responsive login screen design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,7 @@ import CompanyLogo from "../assets/icons/igsas-logo.png";
 import LoginImage from "../assets/images/login-image.jpg";
 
 const Home = () => {
-  return (
-    <div className=" relative">
-      <img src={CompanyLogo} alt="Company Logo" />
-      <div className="flex border h-screen">
-        <div className="w-[70%] h-full">
-          <img src={LoginImage} alt="Login Illustration" className="n h-full" />
-        </div>
-        <div className="w-[30%]">
-          <h2>{`İgsaş Bildirim \n Arayüzüne Hoş Geldiniz`}</h2>
-        </div>
-      </div>
-    </div>
-  );
+  return <h1>Home</h1>;
 };
 
 export default Home;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import CompanyLogo from "../assets/icons/igsas-logo.png";
+import LoginImage from "../assets/images/login-image.jpg";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="relative h-screen overflow-hidden">
+      <div className="px-10 py-4">
+        <img src={CompanyLogo} alt="Company Logo" />
+      </div>
+      <div className="flex h-full">
+        <div className="hidden sm:block w-[60%] lg:w-[70%] h-full">
+          <img
+            src={LoginImage}
+            alt="Login Illustration"
+            className="h-full w-full object-contain"
+          />
+        </div>
+        <div className="w-full sm:w-[40%] lg:w-[30%] relative bg-[#d6d6d618] items-center justify-center flex">
+          <div className="mb-20 flex flex-col gap-4 items-center justify-center h-full  w-[75%]">
+            <h2 className="font-medium text-3xl text-black w-full">
+              İgsaş Bildirim <br /> Arayüzüne Hoş Geldiniz
+            </h2>
+            <div className="flex flex-col gap-4 w-full">
+              <div className="flex flex-col w-full">
+                <label className=" text-gray-500" htmlFor="username">
+                  E-Posta
+                </label>
+                <input
+                  type="text"
+                  label="john.doe@example.com"
+                  placeholder="Kullanıcı Adı"
+                  className="border border-gray-300 rounded-md p-2"
+                />
+              </div>
+              <div className="flex flex-col w-full">
+                <div className="flex justify-between w-full">
+                  <label className=" text-gray-500" htmlFor="username">
+                    Şifre
+                  </label>
+                  <p className="text-sm text-gray-500">Şifremi Unuttum</p>
+                </div>
+                <input
+                  type="password"
+                  placeholder="Şifrenizi giriniz"
+                  className="border border-gray-300 rounded-md p-2"
+                />
+              </div>
+              <div className="flex items-center gap-2 w-full">
+                <input type="checkbox" className="w-5 h-5" />
+                <label className=" text-gray-500" htmlFor="username">
+                  Beni Hatırla
+                </label>{" "}
+              </div>
+              <button className="bg-[#0E5239] font-light text-white p-2 rounded-md">
+                Giriş
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
