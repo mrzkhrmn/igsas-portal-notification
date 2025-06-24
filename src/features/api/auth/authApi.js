67a29ebd675b7eab0d6c05043b3143b33b787fa3
mrzkhrmn <mrzkhrmn@hotmail..com>
2025-06-24 17:43:28 +0300

    rtk query setup

---
+++ src/features/api/auth/authApi.js
@@ -0,0 +1,13 @@
+import { baseApi } from "../baseApi";
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+  }),
+});
--- store.js
+++ store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import globalReducer from "./src/features/global/globalSlice";
+import { baseApi } from "./src/features/api/baseApi";
 
 export const store = configureStore({
   reducer: {
     global: globalReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
